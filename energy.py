ae63989110aac7af846dfaee09f588c6244fc54a
Andrea <aed2655@truman.edu>
2012-04-12 19:52:24 -0500

    The implementation of the simulated annealing function

--- energy.py
+++ energy.py
@@ -1,5 +1,6 @@
 from weightothello import *
 from othelloplayers import *
+from random import *
 
 def energy(weights):
     playerList = [OpheliaA]
@@ -16,3 +17,32 @@ def energy(weights):
 			  player("THANG_W"),
 			  weights)
     return E
+
+def simulatedannealing(temperature, energy, emax, kmax):
+    initial_state = randomstate()
+    e = energy
+    e_best = e
+    s_best = initial_state
+    k = 0
+    while k < kmax and e > emax:
+        t = temperature(k/kmax)
+        s_new = randomstate()
+        e_new = energy(s_new)
+        delta_energy = e_new - e
+        if random.random() < exp(-delta_energy/t):
+             e = e_new
+             s = s_new
+        if e < e_best:
+            s_best = s_new
+            e_best = e_new
+        k += 1
+    return s_best
+        
+        
+        
+        
+    
+def randomstate:
+    return (random.random(), random.random(), random.random())
+
+ 
